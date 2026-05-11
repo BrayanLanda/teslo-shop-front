c061379f15f5513d9739ea7245d245cbda23c242
Brayan Landa <landakkpp@gmail.com>
2026-05-11 00:24:41 -0500

    feat: add environment base URLs, pagination interface, and product retrieval service method

---
+++ src/app/product/interfaces/pagination.interface.ts
@@ -0,0 +1,5 @@
+interface Pagination {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
--- src/app/product/services/product.ts
+++ src/app/product/services/product.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { ProductResponse } from '../interfaces/product-response.interface';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root',
 })
 export class Product {
-  
+  private http = inject(HttpClient);
+
+  getProducts(options: Pagination): Observable<ProductResponse> {
+    const { gender = '', limit = 10, offset = 0 } = options;
+
+    return this.http
+      .get<ProductResponse>(`${baseUrl}/products`, {
+        params: {
+          gender,
+          limit,
+          offset,
+        },
+      })
+      .pipe(
+        tap((resp) => {
+          console.log(resp);
+        }),
+      );
+  }
 }
