2f516d9af249dbffb8f1c67e18792f91d2512f3c
Brayan Landa <landakkpp@gmail.com>
2026-05-12 23:41:58 -0500

    feat: integrate dynamic product data into product card and home page listing

--- src/app/store-front/components/product-card/product-card.ts
+++ src/app/store-front/components/product-card/product-card.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../../product/interfaces/product-response.interface';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
 export class ProductCard {
-
+  product = input.required<Product>();
 }
